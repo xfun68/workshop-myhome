59a956bf31fc5fe4c7c35834bdf63519bcb8d5cc
Xiaofeng Wang <xfwang@thoughtworks.com>
2011-09-18 21:04:25 +0800

    Pull the latest version before installing

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Pulling the latest version..."
 git pull
 
+echo "Installing..."
 for src_file in "$PWD/.bashrc $PWD/.gitconfig"
 do
   ln -sbv $src_file ~
 done
+
+echo "Done"
